57124f2829531b6b6d497dadc3e9f86bd89d6b75
hernian <hernianrunner@gmail.com>
2025-10-09 15:50:38 +0900

    TimerQueueTimerのコールバックでのRace Condition問題の修正 クリックロック状態のUIへの反映 クリックロック状態になったときに効果音を再生する

--- MouseClickLocker/MarkerForm.cs
+++ MouseClickLocker/MarkerForm.cs
@@ -1,56 +1,103 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows.Forms;
+using static MouseClickLocker.Win32Api;
+using static MouseClickLocker.MouseHookLib;
+using System.Media;
 
 namespace MouseClickLocker
 {
     public partial class MarkerForm : LayeredWindow
     {
-        public enum LockState
-        {
-            None,
-            Left,
-            Right,
-            Both
-        }
-
-        private readonly Dictionary<LockState, Bitmap> _lockBitmatDict = new()
-        {
-            { LockState.None, Properties.Resources.LockImageLeft },
-            { LockState.Left, Properties.Resources.LockImageLeft },
-            { LockState.Right, Properties.Resources.LockImageRight },
-            { LockState.Both, Properties.Resources.LockImageBoth }
-        };
+        private readonly Bitmap _lockImageLeft = Properties.Resources.LockImageLeft;
+        private readonly Bitmap _lockImageRight = Properties.Resources.LockImageRight;
+        private readonly Bitmap _lockImageBoth = Properties.Resources.LockImageBoth;
+        private Bitmap _layeredBitmap;
+        private bool _isLeftButtonLocked = false;
+        private bool _isRightButtonLocked = false;
+        private readonly SoundPlayer _soundPlayer;
 
         public MarkerForm()
         {
             InitializeComponent();
             this.AutoScaleMode = AutoScaleMode.None;
             this.Enabled = false;
+            this.Visible = false;
             this.Load += MarkerForm_Load;
+
+            _layeredBitmap = _lockImageLeft;
+            _soundPlayer = new SoundPlayer(Properties.Resources.ClickLockONSound);
         }
 
         private void MarkerForm_Load(object? sender, EventArgs e)
         {
-            var bitmap = _lockBitmatDict[LockState.None];
-            this.Size = bitmap.Size;
-            this.SetLayeredBitmap(bitmap);
+            this.Size = _layeredBitmap.Size;
+            this.SetLayeredBitmap(_layeredBitmap);
         }
 
-        public void SetLockState(LockState state)
+        private Bitmap? GetLockStateBitmap()
         {
-            var bitmap = _lockBitmatDict[state];
-            this.SetLayeredBitmap(bitmap);
+            if (this._isLeftButtonLocked && this._isRightButtonLocked)
+            {
+                return _lockImageBoth;
+            }
+            if (this._isLeftButtonLocked)
+            {
+                return _lockImageLeft;
+            }
+            if (this._isRightButtonLocked)
+            {
+                return _lockImageRight;
+            }
+            return null;
+        }
+
+        private void UpdateLayeredBitmap()
+        {
+            var bitmap = this.GetLockStateBitmap();
+            if (bitmap != null)
+            {
+                if (_layeredBitmap != bitmap)
+                {
+                    _layeredBitmap = bitmap;
+                    this.SetLayeredBitmap(_layeredBitmap);
+                }
+                this.Visible = true;
+            }
+            else
+            {
+                this.Visible = false;
+            }
+        }
+
+        private void OnNotifyLockState(ref Message m)
+        {
+            switch (m.WParam)
+            { 
+                case LOCK_STATE_LEFT:
+                    this._isLeftButtonLocked = (m.LParam == 1);
+                    break;
+                case LOCK_STATE_RIGHT:
+                    this._isRightButtonLocked = (m.LParam == 1);
+                    break;
+            }
+            if (m.LParam == 1)
+            {
+                _soundPlayer.Play();
+            }
+            this.UpdateLayeredBitmap();
         }
 
         protected override void WndProc(ref Message m)
         {
-            const int WM_NCHITTEST = 0x0084;
-            const int HTCAPTION = 0x02;
-
             if (m.Msg == WM_NCHITTEST)
             {
                 m.Result = (IntPtr)HTCAPTION;
+                return;
+            }
+            if (m.Msg == WM_NOTIFY_LOCK_STATE)
+            {
+                this.OnNotifyLockState(ref m);
                 return;
             }
             base.WndProc(ref m);
---
+++ MouseClickLocker/sound/ReadMe.txt
@@ -0,0 +1,8 @@
+著作権
+音楽：魔王魂
+
+以下のURLから「システム12 wav」をダウンロードしてください
+https://maou.audio/se_system12/
+
+利用規約
+https://maou.audio/rule/
